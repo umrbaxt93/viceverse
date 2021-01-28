56df1b37ad04924b65c5884fdb5e192612211ffd
Umidjon Fatullaev <umrbaxt93@gmail.com>
2021-01-28 14:33:37 +0500

    Add Word count

--- viceverse/views.py
+++ viceverse/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 def home(request):
 
     print(request.POST)
@@ -9,6 +8,11 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    words = user_text.split()
+    number_of_words = len(words)
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    reversed_words = reversed_text.split()
+    reversed_words_lens = len(reversed_words)
+
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'number_of_words': number_of_words, 'reversed_words':reversed_words_lens})
 
